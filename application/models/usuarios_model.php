607b42a70d0ca5024eabe44f4d6faf75e8b437ff
AleCentalz <palkya58@gmail.com>
2021-07-02 13:33:42 -0500

    Rename

--- application/models/usuarios_model.php
+++ application/models/usuarios_model.php
@@ -3,11 +3,6 @@ defined('BASEPATH') or exit('No direct script access allowed');
 
 class Usuarios_model extends CI_Model
 {
-	function __construct()
-	{
-		parent::__construct();
-	}
-
 	public function get_usuario($email, $pass)
 	{
 		$this->db->where('email', $email);
