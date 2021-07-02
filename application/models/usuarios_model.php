1b5a43263c4db2c4d712f027d1ca7ac418084918
AleCentalz <palkya58@gmail.com>
2021-07-02 11:20:55 -0500

    Add construct

--- application/models/usuarios_model.php
+++ application/models/usuarios_model.php
@@ -1,15 +1,19 @@
 <?php
-defined('BASEPATH') OR exit('No direct script access allowed');
+defined('BASEPATH') or exit('No direct script access allowed');
 
-class Usuarios_model extends CI_Model {
+class Usuarios_model extends CI_Model
+{
+	function __construct()
+	{
+		parent::__construct();
+	}
 
 	public function get_usuario($email, $pass)
 	{
-        $this->db->where('email',$email);
-        $this->db->where('pass',$pass);
-		$query = $this->db->get('usuarios');  
-		
-		return $query->row();      
-	}
+		$this->db->where('email', $email);
+		$this->db->where('pass', $pass);
+		$query = $this->db->get('usuarios');
 
+		return $query->row();
+	}
 }
