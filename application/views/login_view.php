e17445dff633cb0830eb5418bb847341362d024b
AleCentalz <palkya58@gmail.com>
2021-07-02 13:13:37 -0500

    Change title

--- application/views/login_view.php
+++ application/views/login_view.php
@@ -7,7 +7,7 @@ defined('BASEPATH') or exit('No direct script access allowed');
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Signin Template · Bootstrap v5.0</title>
+    <title>Computo en la Nube Proyecto U5</title>
     <!-- Bootstrap CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-+0n0xVW2eSR5OomGNYDnhzAbDsOXxcvSN1TPprVMTNDbiYZCxYbOOl7+AMvyTG2x" crossorigin="anonymous">
     <!-- Custom styles for this template -->
